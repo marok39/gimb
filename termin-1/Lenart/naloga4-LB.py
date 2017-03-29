0427ac80b850774cabce93f984b5883c5c248e26
LenartBucar <lenart.bucar@dijaki.gimb.org>
2017-03-29 15:10:31 +0200

    Add base file for ex. 4

---
+++ termin-1/Lenart/naloga4-LB.py
@@ -0,0 +1,18 @@
+import sys
+
+def funkcija(a, b):
+    return 0
+
+def main():
+    if len(sys.argv) > 1:
+        a = int(sys.argv[1])
+        b = int(sys.argv[2])
+    else:
+        a = int(input())
+        b = int(input())
+    
+    n = funkcija(a, b)
+    print("%d" % n)
+
+if __name__ == "__main__":
+    main()
