d6d35b54bdc5834b668592ade37eec0a61d41e02
LenartBucar <lenart.bucar@dijaki.gimb.org>
2017-03-29 16:10:54 +0200

    Work on ex. 4

--- termin-1/Lenart/naloga4-LB.py
+++ termin-1/Lenart/naloga4-LB.py
@@ -1,7 +1,15 @@
 import sys
 
-def funkcija(a, b):
-    return 0
+def funkcija(a, b, x):
+    #a -= 1
+    #b += 1
+    delta = b - a
+    amount = delta//x
+    if a % x == 0:
+        amount += 1
+    if b % x == 0:
+        amount += 1
+    return amount
 
 def main():
     if len(sys.argv) > 1:
@@ -11,7 +19,7 @@ def main():
         a = int(input())
         b = int(input())
 
-    n = funkcija(a, b)
+    n = funkcija(a, b, 2) + funkcija(a, b, 3) + funkcija(a, b, 5) - funkcija(a, b, 6) - funkcija(a, b, 10) - funkcija(a, b, 12) + funkcija(a, b, 30)
     print("%d" % n)
 
 if __name__ == "__main__":
